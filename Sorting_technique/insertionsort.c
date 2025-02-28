ef08bc0e2d81bb3f7ae900a5db8a4f0cb957ad9c
vanshaj bansal <bnslgvansh67@gmail.com>
2025-03-01 00:23:17 +0530

    done

--- Sorting_technique/insertionsort.c
+++ Sorting_technique/insertionsort.c
@@ -30,18 +30,19 @@ int display(int array[], int n)
     printf("\n");
 }
 
-int Insertionsort(int arr[], int size)
+int Insertionsort(int arr[], int size) // selection sort me aur isme ye parameters same hai function call me
 {
     int key, j;
     for (int i = 1; i < size; i++)
     {
         key = arr[i];
         j = i - 1;
-        while (j >= 0 && key < arr[j])
+        while (j >= 0 && key < arr[j]) // while loop isliye lagaya kyuki hume key se phele se saare element compare krne uske liye(j>=0) lagaya aur saath ki saath ye bhi compare krlia ki key se phele ka koi element bda to ni hai
+        // agar key se phele ka koi element bada hua
         {
-            arr[j + 1] = arr[j];
-            j--;
+            arr[j + 1] = arr[j]; // to hume key ke andar us value ko daalna hai
+            j--;                 // aur fir j ku ghataya isliye hai ki kahi usse chota bhi koi element to ni hai
         }
-        arr[j + 1] = key;
+        arr[j + 1] = key; // aur jab while loop false hoega to is condition ki help se hum key ko uski original jangeh pe set krdenge
     }
 }
--- Sorting_technique/mergesort.c
+++ Sorting_technique/mergesort.c
@@ -38,48 +38,48 @@ int MergeSort(int arr[], int low, int high)
     if (low < high)
     {
         int mid = low + (high - low) / 2;
-        MergeSort(arr, low, mid);
-        MergeSort(arr, mid + 1, high);
+        MergeSort(arr, low, mid);      // is function call se hum first half ko aur bhi divide krre
+        MergeSort(arr, mid + 1, high); // is function call ki help se hum secod half ko divide krre
 
-        Merge(arr, low, mid, high);
+        Merge(arr, low, mid, high); // yo function call sabse phele 2 element ko sort krra fir 4 element ko fir 8 ko.....
     }
 }
 int Merge(int arr[], int left, int mid, int right)
 {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    int l1[n1], l2[n2];
+    int n1 = mid - left + 1; // first half ka array banane ke liye size calculate kia
+    int n2 = right - mid;    // second half ka array banane ke liye size calculate kia
+    int l1[n1], l2[n2];      // yha bhi dono array initialize kre ....inko hum L and R naam bhi bna sake vo sahi option hai l1 aur l2 thodi se confusion krdega
     for (int j = 0; j < n1; j++)
     {
-        l1[j] = arr[left + j];
+        l1[j] = arr[left + j]; // is puure for loop se first half ki value l1(L) me daali
     }
     for (int j = 0; j < n2; j++)
     {
-        l2[j] = arr[mid + 1 + j];
+        l2[j] = arr[mid + 1 + j]; // is loop se socnd half ki value l2(R)me
     }
 
     int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2)
+    while (i < n1 && j < n2) // is loop se hum dono halfs ko tab tak traverse krenge jab tak ki dono puure traverse ni ho jaate
     {
         if (l1[i] <= l2[j])
         {
-            arr[k] = l1[i];
-            i++;
+            arr[k] = l1[i]; // is if aur else se humne jo original array se usme upate kra
+            i++;            // matlab agar l1 ka phela element l2 ke phele element se chota hai to usko original waale me add krdia
         }
         else
         {
-            arr[k] = l2[j];
+            arr[k] = l2[j]; // isme vise versa
             j++;
         }
         k++;
     }
-    while (i < n1)
+    while (i < n1) // is loop se bache hue saare element l1 ke original waale me add kre
     {
         arr[k] = l1[i];
         i++;
         k++;
     }
-    while (j < n2)
+    while (j < n2) // aur is loop se l2 ke bache hue element original waale me add kre
     {
         arr[k] = l2[j];
         j++;
